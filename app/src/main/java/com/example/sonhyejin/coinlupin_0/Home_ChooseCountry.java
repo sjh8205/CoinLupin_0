dab3e24f276fe73132e0cc39ebf3be41931e4972
sonhyejin <sjh8205@naver.com>
2018-09-19 17:13:05 +0900

    Initial commit

---
+++ app/src/main/java/com/example/sonhyejin/coinlupin_0/Home_ChooseCountry.java
@@ -0,0 +1,13 @@
+package com.example.sonhyejin.coinlupin_0;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home_ChooseCountry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home__choose_country);
+    }
+}
