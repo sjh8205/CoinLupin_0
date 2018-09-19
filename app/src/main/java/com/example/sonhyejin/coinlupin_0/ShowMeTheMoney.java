4880e0ec529149f22e6880d938a76519f6c642ea
sonhyejin <sjh8205@naver.com>
2018-09-19 17:54:15 +0900

    setting_first

---
+++ app/src/main/java/com/example/sonhyejin/coinlupin_0/ShowMeTheMoney.java
@@ -0,0 +1,13 @@
+package com.example.sonhyejin.coinlupin_0;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowMeTheMoney extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_me_the_money);
+    }
+}
