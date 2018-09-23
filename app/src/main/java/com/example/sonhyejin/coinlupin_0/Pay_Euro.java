4746ca67cef551a4a5a6e6407368932deb0e5d98
sonhyejin <sjh8205@naver.com>
2018-09-23 19:16:10 +0900

    Rename_Pay_java_and_xml_file
    Pay
    --> Pay_Euro.java
    --> activity_pay_euro.xml

--- app/src/main/java/com/example/sonhyejin/coinlupin_0/Pay_Euro.java
+++ app/src/main/java/com/example/sonhyejin/coinlupin_0/Pay_Euro.java
@@ -3,11 +3,11 @@ package com.example.sonhyejin.coinlupin_0;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Pay extends AppCompatActivity {
+public class Pay_Euro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pay);
+        setContentView(R.layout.activity_pay_euro);
     }
 }
