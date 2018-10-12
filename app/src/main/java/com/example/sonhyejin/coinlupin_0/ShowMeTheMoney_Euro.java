b1c32946997159ee4c7e27e3b018adb31da954d4
sonhyejin <sjh8205@naver.com>
2018-10-12 10:27:42 +0900

    윤지 수정

--- app/src/main/java/com/example/sonhyejin/coinlupin_0/ShowMeTheMoney_Euro.java
+++ app/src/main/java/com/example/sonhyejin/coinlupin_0/ShowMeTheMoney_Euro.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 
 public class ShowMeTheMoney_Euro extends AppCompatActivity {
 
-    public void Reset_CoinCount(){
+    public void Reset_CoinCount(){       // reset 버튼 눌렀을 때 호출되는 reset 함수
         SharedPreferences shared = getSharedPreferences("name", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = shared.edit();
 
@@ -30,7 +30,7 @@ public class ShowMeTheMoney_Euro extends AppCompatActivity {
         editor.putInt("cent10000",0);
         editor.putInt("cent20000",0);
         editor.putInt("cent50000",0);
-        editor.putFloat("Total_money",0);
+        editor.putFloat("total_money",0);
         editor.apply();
 
     }
@@ -42,101 +42,109 @@ public class ShowMeTheMoney_Euro extends AppCompatActivity {
         setContentView(R.layout.activity_show_me_the_money__euro);
 
 
-        TextView Total_Money;
-        TextView ECoin1,ECoin2,ECoin3,ECoin4,ECoin5,ECoin6,ECoin7,ECoin8,ECoin9,ECoin10,ECoin11,ECoin12,ECoin13,ECoin14,ECoin15;
+        TextView Total_Money;    // 총 보유 금액 보여주는 textview 변수 Total_Money 선언
+        TextView ECoin1,ECoin2,ECoin3,ECoin4,ECoin5,ECoin6,ECoin7,ECoin8,ECoin9,ECoin10,ECoin11,ECoin12,ECoin13,ECoin14,ECoin15;  // 각 동전 개수 보여주는 texetview 변수 선언
 
-        Button gotoReset = (Button) findViewById(R.id.resetB);
-        Button gotoPay = (Button) findViewById(R.id.payB);
-        Button gotoEdit = (Button) findViewById(R.id.editB);
+        Button gotoReset = (Button) findViewById(R.id.resetB);   // reset 버튼 변수 gotoReset 선언
+        Button gotoPay = (Button) findViewById(R.id.payB);       // pay 버튼 변수 gotoPay 선언
+        Button gotoEdit = (Button) findViewById(R.id.editB);     // edit 버튼 변수 gotoEdit 선언
 
         SharedPreferences shared = getSharedPreferences("name", Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit = shared.edit();
 
         //SharedPreferences sp = getSharedPreferences("shared",MODE_PRIVATE);  //string name : shared
-        Float OwnMoney = shared.getFloat("Total_money", -1);  //  총 금액 값 key : Total_money
-        Total_Money = (TextView)findViewById(R.id.Total_Money);
-        Total_Money.setText(Float.toString(OwnMoney));  // SharedPreference에 저장되어있는 값 Total_money 출력
+        //Float OwnMoney = shared.getFloat("Total_money", -1);  //  총 금액 값 key : Total_money
 
-        int Ecoin1 = shared.getInt("cent1", -1); // ECoin1 : 유로코인1 (제일 싼 동전)
+
+        int Ecoin1 = shared.getInt("cent1", -1); // ECoin1 : 유로코인1 (1cent)
         ECoin1 = (TextView)findViewById(R.id.HMcoin1);
         ECoin1.setText(Integer.toString(Ecoin1));
 
-        int Ecoin2 = shared.getInt("cent2", -1); // ECoin2 : 유로코인2 (두번째로 싼 동전)
+        int Ecoin2 = shared.getInt("cent2", -1); // ECoin2 : 유로코인2 (2cent)
         ECoin2 = (TextView)findViewById(R.id.HMcoin2);
         ECoin2.setText(Integer.toString(Ecoin2));
 
-        int Ecoin3 = shared.getInt("cent5", -1); // ECoin3 : 유로코인3 (세번째로 싼 동전)
+        int Ecoin3 = shared.getInt("cent5", -1); // ECoin3 : 유로코인3 (5cent)
         ECoin3 = (TextView)findViewById(R.id.HMcoin3);
         ECoin3.setText(Integer.toString(Ecoin3));
 
-        int Ecoin4 = shared.getInt("cent10", -1); // ECoin4 : 유로코인4
+        int Ecoin4 = shared.getInt("cent10", -1); // ECoin4 : 유로코인4 (10cent)
         ECoin4 = (TextView)findViewById(R.id.HMcoin4);
         ECoin4.setText(Integer.toString(Ecoin4));
 
-        int Ecoin5 = shared.getInt("cent20", -1); // ECoin5 : 유로코인5
+        int Ecoin5 = shared.getInt("cent20", -1); // ECoin5 : 유로코인5 (20cent)
         ECoin5 = (TextView)findViewById(R.id.HMcoin5);
         ECoin5.setText(Integer.toString(Ecoin5));
 
-        int Ecoin6 = shared.getInt("cent50", -1); // ECoin6 : 유로코인6
+        int Ecoin6 = shared.getInt("cent50", -1); // ECoin6 : 유로코인6 (50cent)
         ECoin6 = (TextView)findViewById(R.id.HMcoin6);
         ECoin6.setText(Integer.toString(Ecoin6));
 
-        int Ecoin7 = shared.getInt("cent100", -1); // ECoin7 : 유로코인7
+        int Ecoin7 = shared.getInt("cent100", -1); // ECoin7 : 유로코인7 (1euro == 100cent)
         ECoin7 = (TextView)findViewById(R.id.HMcoin7);
         ECoin7.setText(Integer.toString(Ecoin7));
 
-        int Ecoin8 = shared.getInt("cent200", -1); // ECoin8 : 유로코인8
+        int Ecoin8 = shared.getInt("cent200", -1); // ECoin8 : 유로코인8 (2euro == 200cent)
         ECoin8 = (TextView)findViewById(R.id.HMcoin8);
         ECoin8.setText(Integer.toString(Ecoin8));
 
-        int Ecoin9 = shared.getInt("cent500", -1); // ECoin9 : 유로코인9
+        int Ecoin9 = shared.getInt("cent500", -1); // ECoin9 : 유로코인9 (5euro == 500cent)
         ECoin9 = (TextView)findViewById(R.id.HMcoin9);
         ECoin9.setText(Integer.toString(Ecoin9));
 
-        int Ecoin10 = shared.getInt("cent1000", -1); // ECoin10 : 유로코인10
+        int Ecoin10 = shared.getInt("cent1000", -1); // ECoin10 : 유로코인10 (10euro == 1000cent)
         ECoin10 = (TextView)findViewById(R.id.HMcoin10);
         ECoin10.setText(Integer.toString(Ecoin10));
 
-        int Ecoin11 = shared.getInt("cent2000", -1); // ECoin11 : 유로코인11
+        int Ecoin11 = shared.getInt("cent2000", -1); // ECoin11 : 유로코인11 (20euro == 2000cent)
         ECoin11 = (TextView)findViewById(R.id.HMcoin11);
         ECoin11.setText(Integer.toString(Ecoin11));
 
-        int Ecoin12 = shared.getInt("cent5000", -1); // ECoin12 : 유로코인12
+        int Ecoin12 = shared.getInt("cent5000", -1); // ECoin12 : 유로코인12 (50euro == 5000cent)
         ECoin12 = (TextView)findViewById(R.id.HMcoin12);
         ECoin12.setText(Integer.toString(Ecoin12));
 
-        int Ecoin13 = shared.getInt("cent10000", -1); // ECoin13 : 유로코인13
+        int Ecoin13 = shared.getInt("cent10000", -1); // ECoin13 : 유로코인13 (100euro == 10000cent)
         ECoin13 = (TextView)findViewById(R.id.HMcoin13);
         ECoin13.setText(Integer.toString(Ecoin13));
 
-        int Ecoin14 = shared.getInt("cent20000", -1); // ECoin14 : 유로코인14
+        int Ecoin14 = shared.getInt("cent20000", -1); // ECoin14 : 유로코인14 (200euro == 20000cent)
         ECoin14 = (TextView)findViewById(R.id.HMcoin14);
         ECoin14.setText(Integer.toString(Ecoin14));
 
-        int Ecoin15 = shared.getInt("cent50000", -1); // ECoin15 : 유로코인15
+        int Ecoin15 = shared.getInt("cent50000", -1); // ECoin15 : 유로코인15 (500euro == 50000cent)
         ECoin15 = (TextView)findViewById(R.id.HMcoin15);
         ECoin15.setText(Integer.toString(Ecoin15));
 
-        gotoReset.setOnClickListener(new View.OnClickListener() {
+        float OwnMoney = (float)((1*Ecoin1) + 2*Ecoin2 + 5*Ecoin3 + Ecoin4*10 + Ecoin5*20
+                + Ecoin6*50 + Ecoin7*100 + Ecoin8*200 + Ecoin9*500 + Ecoin10*1000
+                + Ecoin11*2000 + Ecoin12*5000 + Ecoin13*10000 + Ecoin14*20000 + Ecoin15*50000);  //총 보유금액 OwnMoney에 저장
+        Total_Money = (TextView)findViewById(R.id.Total_Money); //id가 Total_Money인 textview ---> 변수 Total_Money라고 선언
+        Total_Money.setText(Float.toString(OwnMoney));  //textview에 OwnMoney출력
+        edit.putFloat("total_money",OwnMoney);  //총 보유 금액 key 값 : total_money, SharedPreference에 OwnMoney 저장
+        edit.apply();
+
+        gotoReset.setOnClickListener(new View.OnClickListener() {     //reset 버튼 눌렀을 때
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(ShowMeTheMoney_Euro.this, Home_ChooseCountry.class);
-                Reset_CoinCount();   // reset 함수 ---->
+                Intent intent = new Intent(ShowMeTheMoney_Euro.this, Home_ChooseCountry.class);  //intent() 함수 이용해서 화면 전환
+                Reset_CoinCount();   // reset 함수 호출해서 각 동전 개수 & 총 보유금액 0으로 갱신
                 startActivity(intent);
             }
         });
 
-        gotoEdit.setOnClickListener(new View.OnClickListener() {
+        gotoEdit.setOnClickListener(new View.OnClickListener() {     //edit 버튼 눌렀을 때
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(ShowMeTheMoney_Euro.this, Edit_Euro.class);
+                Intent intent = new Intent(ShowMeTheMoney_Euro.this, Edit_Euro.class);  //intent() 함수 이용해서 화면 전환
                 startActivity(intent);
             }
         });
 
-        gotoPay.setOnClickListener(new View.OnClickListener() {
+        gotoPay.setOnClickListener(new View.OnClickListener() {      //pay 버튼 눌렀을 때
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(ShowMeTheMoney_Euro.this, Pay_Euro.class);
+                Intent intent = new Intent(ShowMeTheMoney_Euro.this, Pay_Euro.class);  //intent() 함수 이용해서 화면 전환
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/sonhyejin/coinlupin_0/ShowMeTheMoney_Yen.java
+++ app/src/main/java/com/example/sonhyejin/coinlupin_0/ShowMeTheMoney_Yen.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 
 public class ShowMeTheMoney_Yen extends AppCompatActivity {
 
-    public void Reset_CoinCount(){
+    public void Reset_CoinCount(){       // reset 버튼 눌렀을 때 호출되는 reset 함수
         SharedPreferences shared = getSharedPreferences("name", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = shared.edit();
 
@@ -25,7 +25,7 @@ public class ShowMeTheMoney_Yen extends AppCompatActivity {
         editor.putInt("yen2000", 0);
         editor.putInt("yen5000", 0);
         editor.putInt("yen10000", 0);
-        editor.putFloat("Total_money",0);
+        editor.putFloat("total_money",0);
         editor.apply();
 
     }
@@ -35,79 +35,88 @@ public class ShowMeTheMoney_Yen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_me_the_money_yen);
 
-        TextView Total_Money;
-        TextView YCoin1,YCoin2,YCoin3, YCoin4,YCoin5,YCoin6,YCoin7,YCoin8,YCoin9,YCoin10;
+        TextView Total_Money;    // 총 보유 금액 보여주는 textview 변수 Total_Money 선언
+        TextView YCoin1,YCoin2,YCoin3, YCoin4,YCoin5,YCoin6,YCoin7,YCoin8,YCoin9,YCoin10;    // 각 동전 개수 보여주는 texetview 변수 선언
 
-        Button gotoReset = (Button) findViewById(R.id.resetB);
-        Button gotoPay = (Button) findViewById(R.id.payB);
-        Button gotoEdit = (Button) findViewById(R.id.editB);
+        Button gotoReset = (Button) findViewById(R.id.resetB);     // reset 버튼 변수 gotoReset 선언
+        Button gotoPay = (Button) findViewById(R.id.payB);         // pay 버튼 변수 gotoPay 선언
+        Button gotoEdit = (Button) findViewById(R.id.editB);       // edit 버튼 변수 gotoEdit 선언
 
         SharedPreferences shared = getSharedPreferences("name", Context.MODE_PRIVATE);
-        Float OwnMoney = shared.getFloat("Total_money", -1);  //  총 금액 값 key : Total_money
-        Total_Money = (TextView)findViewById(R.id.Total_Money);
-        Total_Money.setText(Float.toString(OwnMoney));  // SharedPreference에 저장되어있는 값 Total_money 출력
+        SharedPreferences.Editor edit = shared.edit();
 
-        int Ycoin1 = shared.getInt("yen1", -1); // YCoin1 : 엔코인1 (제일 싼 동전)
+        //    Float OwnMoney = shared.getFloat("Total_money", -1);  //  총 금액 값 key : Total_money
+        //    Total_Money = (TextView)findViewById(R.id.Total_Money);
+        //    Total_Money.setText(Float.toString(OwnMoney));  // SharedPreference에 저장되어있는 값 Total_money 출력
+
+        int Ycoin1 = shared.getInt("yen1", -1); // YCoin1 : 엔코인1 (1yen)
         YCoin1 = (TextView)findViewById(R.id.HMcoin1);
         YCoin1.setText(Integer.toString(Ycoin1));
 
-        int Ycoin2 = shared.getInt("yen5", -1); // YCoin2 : 엔코인2 (두번째로 싼 동전)
+        int Ycoin2 = shared.getInt("yen5", -1); // YCoin2 : 엔코인2 (5yen)
         YCoin2 = (TextView)findViewById(R.id.HMcoin2);
         YCoin2.setText(Integer.toString(Ycoin2));
 
-        int Ycoin3 = shared.getInt("yen10", -1); // YCoin3 : 엔코인3 (세번째로 싼 동전)
+        int Ycoin3 = shared.getInt("yen10", -1); // YCoin3 : 엔코인3 (10yen)
         YCoin3 = (TextView)findViewById(R.id.HMcoin3);
         YCoin3.setText(Integer.toString(Ycoin3));
 
-        int Ycoin4 = shared.getInt("yen50", -1); // YCoin4 : 엔코인4
+        int Ycoin4 = shared.getInt("yen50", -1); // YCoin4 : 엔코인4 (50yen)
         YCoin4 = (TextView)findViewById(R.id.HMcoin4);
         YCoin4.setText(Integer.toString(Ycoin4));
 
-        int Ycoin5 = shared.getInt("yen100", -1); // YCoin5 : 엔코인5
+        int Ycoin5 = shared.getInt("yen100", -1); // YCoin5 : 엔코인5 (100yen)
         YCoin5 = (TextView)findViewById(R.id.HMcoin5);
         YCoin5.setText(Integer.toString(Ycoin5));
 
-        int Ycoin6 = shared.getInt("yen500", -1); // YCoin6 : 엔코인6
+        int Ycoin6 = shared.getInt("yen500", -1); // YCoin6 : 엔코인6 (500yen)
         YCoin6 = (TextView)findViewById(R.id.HMcoin6);
         YCoin6.setText(Integer.toString(Ycoin6));
 
-        int Ycoin7 = shared.getInt("yen1000", -1); // YCoin7 : 엔코인7
+        int Ycoin7 = shared.getInt("yen1000", -1); // YCoin7 : 엔코인7 (1000yen)
         YCoin7 = (TextView)findViewById(R.id.HMcoin7);
         YCoin7.setText(Integer.toString(Ycoin7));
 
-        int Ycoin8 = shared.getInt("yen2000", -1); // YCoin8 : 엔코인8
+        int Ycoin8 = shared.getInt("yen2000", -1); // YCoin8 : 엔코인8 (2000yen)
         YCoin8 = (TextView)findViewById(R.id.HMcoin8);
         YCoin8.setText(Integer.toString(Ycoin8));
 
-        int Ycoin9 = shared.getInt("yen5000", -1); // YCoin9 : 엔코인9
+        int Ycoin9 = shared.getInt("yen5000", -1); // YCoin9 : 엔코인9  (5000yen)
         YCoin9 = (TextView)findViewById(R.id.HMcoin9);
         YCoin9.setText(Integer.toString(Ycoin9));
 
-        int Ycoin10 = shared.getInt("yen10000", -1); // YCoin10 : 엔코인10
+        int Ycoin10 = shared.getInt("yen10000", -1); // YCoin10 : 엔코인10  (10000yen)
         YCoin10 = (TextView)findViewById(R.id.HMcoin10);
         YCoin10.setText(Integer.toString(Ycoin10));
 
-        gotoReset.setOnClickListener(new View.OnClickListener() {
+        float OwnMoney = (float)((1*Ycoin1) + 5*Ycoin2 + 10*Ycoin3 + Ycoin4*50 + Ycoin5*100
+                + Ycoin6*500 + Ycoin7*1000 + Ycoin8*2000 + Ycoin9*5000 + Ycoin10*10000);  //총 보유금액 OwnMoney에 저장
+        Total_Money = (TextView)findViewById(R.id.Total_Money);   //id가 Total_Money인 textview ---> 변수 Total_Money라고 선언
+        Total_Money.setText(Float.toString(OwnMoney));  //textview에 OwnMoney출력
+        edit.putFloat("total_money",OwnMoney);  //총 보유 금액 key 값 : total_money, SharedPreference에 OwnMoney 저장
+        edit.apply();
+
+        gotoReset.setOnClickListener(new View.OnClickListener() {         //reset 버튼 눌렀을 때
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(ShowMeTheMoney_Yen.this, Home_ChooseCountry.class);
-                Reset_CoinCount();   // reset 함수 ---->
+                Intent intent = new Intent(ShowMeTheMoney_Yen.this, Home_ChooseCountry.class);     //intent() 함수 이용해서 화면 전환
+                Reset_CoinCount();    // reset 함수 호출해서 각 동전 개수 & 총 보유금액 0으로 갱신
                 startActivity(intent);
             }
         });
 
-        gotoEdit.setOnClickListener(new View.OnClickListener() {
+        gotoEdit.setOnClickListener(new View.OnClickListener() {       //edit 버튼 눌렀을 때
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(ShowMeTheMoney_Yen.this, Edit_Euro.class);
+                Intent intent = new Intent(ShowMeTheMoney_Yen.this, Edit_Euro.class);     //intent() 함수 이용해서 화면 전환
                 startActivity(intent);
             }
         });
 
-        gotoPay.setOnClickListener(new View.OnClickListener() {
+        gotoPay.setOnClickListener(new View.OnClickListener() {      //pay 버튼 눌렀을 때
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(ShowMeTheMoney_Yen.this, Pay_Euro.class);
+                Intent intent = new Intent(ShowMeTheMoney_Yen.this, Pay_Euro.class);     //intent() 함수 이용해서 화면 전환
                 startActivity(intent);
             }
         });
@@ -115,4 +124,3 @@ public class ShowMeTheMoney_Yen extends AppCompatActivity {
     }
 
 }
-
