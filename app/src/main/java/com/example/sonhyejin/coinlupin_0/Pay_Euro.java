380b917f7ee6e0c0a699ddccb5b1812905205d40
dlwldnjs950 <dlwldnjs950@naver.com>
2018-11-12 08:49:50 +0900

    센트칸에 5입력시 50으로 처리

--- app/src/main/java/com/example/sonhyejin/coinlupin_0/Pay_Euro.java
+++ app/src/main/java/com/example/sonhyejin/coinlupin_0/Pay_Euro.java
@@ -34,19 +34,23 @@ public class Pay_Euro extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //아무것도 입력하지 않았을 때 예외처리
-                if(euroIN.getText().length()==0 &&euroDE.getText().length()==0){    //두 입력칸 모두 아무것도 입력하지 않았을 경우 입력하도록 유도
-                    Toast.makeText(Pay_Euro.this, "지불 금액을 입력해주세요", Toast.LENGTH_LONG).show();
+                if(euroIN.getText().length()==0 && euroDE.getText().length()==0){    //두 입력칸 모두 아무것도 입력하지 않았을 경우 입력하도록 유도
+                    Toast.makeText(Pay_Euro.this, "값을 입력해주세요", Toast.LENGTH_LONG).show();
                 }else {
                     try{    //int값 입력 예외처리
-                        if (euroIN.getText().length() > 0) {   //길이로 입력의 여부를 판단
+                        if (euroIN.getText().length() > 0) {   //길이로 입력의 여부를 판단-아무것도 입력하지 않았을 때 예외처리를 위해
                             eIN = Integer.parseInt(euroIN.getText().toString());    //textView에 입력된 값이 String형이라 integer값으로 변환
                         } else   //입력된 값이 없을 때 0으로 값을 전달
                             eIN = 0;
 
-                        if (euroDE.getText().length() > 0) {   //길이로 입력의 여부를 판단
+                        if (euroDE.getText().length() > 0) {   //길이로 입력의 여부를 판단-아무것도 입력하지 않았을 때 예외처리를 위해
                             eDE = Integer.parseInt(euroDE.getText().toString());    //textView에 입력된 값이 String형이라 integer값으로 변환
                         } else   //입력된 값이 없을 때 0으로 값을 전달
                             eDE = 0;
+
+                        //센트칸에 5를 입력한 경우 50으로 처리하도록
+                        if(euroDE.getText().length()==1 && eDE<10)  //입력의 길이가 1이고 값이 10보다 작으면 10배를 해준다.
+                            eDE = eDE*10;
 
                         //총 보유 금액을 넘기는 금액을 입력했을 때 toast 메세지띄우기
                         //if(eIN*100+eDE > 10) { //total_money 받아 오기 전에 조건에 따라 toast메세지가 잘 뜨는지 확인 하기 위해 사용했던 구문
